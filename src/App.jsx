008f88cb0b35007c9333b1ce8065086c7ec68293
Wilson Emmanuel <wilsonedikanabasi@gmail.com>
2026-04-01 06:39:40 +0100

    Home-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Home from "./Home";
 
 function App() {
   return (
     <>
     <Header/>
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Home/>
     </>
   );
 }
